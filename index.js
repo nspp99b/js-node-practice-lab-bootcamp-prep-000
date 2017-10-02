cb688cbfc33909ded89ee03f948365a69b155602
Shahin Motia <chromepanthers@gmail.com>
2017-10-02 03:25:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Shahin"
-var height = "74"
+var height = "68.5"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(parseInt(height)+1)
